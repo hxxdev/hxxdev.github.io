acb0d73c73aa892a5b55bc1356ba1c79e18dea81
hxx.dev <hxx.dev@gmail.com>
2025-04-06 22:16:52 +0900

    resolve merge confict with v7.2.4

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743944399',resources: [
+    cacheName: 'chirpy-1743945396',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
