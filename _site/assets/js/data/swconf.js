4de086ccb5e9660673a29f96f862846bbf88d227
hxx.dev <hxx.dev@gmail.com>
2025-04-07 00:23:27 +0900

    regeneration of _site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743952884',resources: [
+    cacheName: 'chirpy-1743952994',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
