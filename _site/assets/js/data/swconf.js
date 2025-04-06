e4d8477a8730ce7829e2132626f2015d5dd45582
hxx.dev <hxx.dev@gmail.com>
2025-04-06 22:02:31 +0900

    commit

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743943957',resources: [
+    cacheName: 'chirpy-1743944399',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
