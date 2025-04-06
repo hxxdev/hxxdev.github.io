eb190b1081cf1e2f1138d34071758f25651e667f
hxx.dev <hxx.dev@gmail.com>
2025-04-06 21:56:29 +0900

    jekyll chirpy change

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743934097',resources: [
+    cacheName: 'chirpy-1743943957',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
