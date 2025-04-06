c236bec50243a736ae02419afd223ab6b37a0011
hxx.dev <hxx.dev@gmail.com>
2025-04-07 00:26:42 +0900

    build deploy script update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743952994',resources: [
+    cacheName: 'chirpy-1743953188',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
