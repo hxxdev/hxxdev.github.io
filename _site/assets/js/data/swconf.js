3b510ddf2b8b382e9e41bc23ba349c8124e22c6b
hxx.dev <hxx.dev@gmail.com>
2025-04-07 23:16:15 +0900

    refactoring

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744034366',resources: [
+    cacheName: 'chirpy-1744034882',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
