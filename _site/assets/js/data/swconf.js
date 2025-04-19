9d955ab24277900c3c51e426bc587b87bce5793f
hxx.dev <hxx.dev@gmail.com>
2025-04-19 21:27:37 +0900

    new post "All about assembly"

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744038655',resources: [
+    cacheName: 'chirpy-1745065638',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
