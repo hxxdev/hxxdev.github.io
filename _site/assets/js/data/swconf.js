a1e557a915a0243cffe209bc8c4197367cb1f85d
hxx.dev <hxx.dev@gmail.com>
2025-04-26 15:16:32 +0900

    Change: favicons

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745646872',resources: [
+    cacheName: 'chirpy-1745647994',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
