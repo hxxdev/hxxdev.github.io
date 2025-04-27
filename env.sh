cafb7b21470fa540a841979356f0e6906665c1f0
hxx.dev <hxx.dev@gmail.com>
2025-04-27 16:27:51 +0900

    Add: env.sh

---
+++ env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PATH_HXX=$(pwd)
+
+alias gopost="cd \"$PATH_HXX\"/_posts"
+alias goasset="cd \"$PATH_HXX\"/_assets"
+alias start="jekyll serve &"
+alias build="jekyll build &"
+alias kill="ps aux | grep jekyll | awk '{print $2}' | xargs kill -9"
+
+echo "HXX ENV SETUP ********************************************"
+echo "gopost  : go to _posts"
+echo "goasset : go to _assets"
+echo "start   : jekyll serve"
+echo "build   : jekyll build"
+echo "kill    : kill localhost server"
+echo "**********************************************************"
