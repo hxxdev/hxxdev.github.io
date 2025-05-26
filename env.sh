e257f646347605298bb906fb47c9490da5070d00
hxx.dev <hxx.dev@gmail.com>
2025-05-27 00:16:05 +0900

    alias godraft

--- env.sh
+++ env.sh
@@ -2,6 +2,7 @@
 export PATH_HXX=$(pwd)
 
 alias gopost="cd \"$PATH_HXX\"/_posts"
+alias godraft="cd \"$PATH_HXX\"/_drafts"
 alias goasset="cd \"$PATH_HXX\"/_assets"
 alias start="jekyll serve &"
 alias build="jekyll build &"
@@ -9,6 +10,7 @@ alias kill="ps aux | grep jekyll | awk '{print \$2}' | xargs kill -9"
 
 echo "HXX ENV SETUP ********************************************"
 echo "gopost  : go to _posts"
+echo "godraft : go to _drafts"
 echo "goasset : go to _assets"
 echo "start   : jekyll serve"
 echo "build   : jekyll build"
